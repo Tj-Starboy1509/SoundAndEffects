a1f6f5d3f2fdf81c7ad1005abbd852b57dcc4661
Tristan Tricoche <tricochet@roverkids.org>
2024-12-16 13:41:49 -0500

    keeps the player from unconscious jumping

--- SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround && !GameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
